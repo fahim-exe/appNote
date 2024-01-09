690c878493d28300ce40c867f957880bf2b5cd51
Mostafa Al Mamun Fahim <67799909+pathetic-coder@users.noreply.github.com>
2024-01-09 14:13:05 +0600

    created database in init.py

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,11 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login', methods=["GET", "POST"])
 def login():
     data = request.form
+   
     return render_template("login.html", boolean=True)
 
 
@@ -12,6 +13,38 @@ def login():
 def logout():
     return "<p>Logout</p>"
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        email = request.form.get("email")
+        firstName = request.form.get("firstName")
+        pass1 = request.form.get("pass1")
+        pass2 = request.form.get("pass2")
+
+        if len(email)<4:
+            flash("Email must be greater than 4 character", category="error")
+
+        elif len(firstName)<2:
+            flash("First name must be greater that 2 characters", category="error")
+        
+        elif pass1 != pass2:
+            flash("Password dont\'t match", category="error")
+
+        elif len(pass1)<7:
+            flash("Password must be equal or greater than 8 characters", category="error")
+
+        else:
+            flash("Account Created!!", category="success")
+
+
+        # print(email, firstName, pass1, pass2)
+        
+
     return render_template("sign_up.html")
+
+
+@auth.route("/home")
+
+def home():
+    
+    return render_template("home.html")
