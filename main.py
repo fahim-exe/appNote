b906ae20238a1a8a80bf3009686004030de689c1
Fahim <fahim180khan@gmail.com>
2024-01-10 22:22:33 +0600

    setted up locally on a differnt pc

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+# i can use git remotely yeeee!!!!!!!!!
+
 from website import create_app
 
 app = create_app()
-# i have added a comment from agar pc check
+
 
 if __name__ =="__main__":
     app.run(debug=True)
